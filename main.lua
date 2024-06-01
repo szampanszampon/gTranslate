94068a1912b8870004f26925371c0913c000af1f
szampan <119541537+szampanszampon@users.noreply.github.com>
2024-06-01 14:08:22 +0200

    Language select boxes work correctly now

--- main.lua
+++ main.lua
@@ -21,18 +21,22 @@ function translate(text, source_lang, target_lang)
   return rest
 end
 
-function comboToIndex(combo)
-    local i = tonumber(combo, 16)
-    return i
+function getLanguageCode(selectedIndex)
+  if selectedIndex < 1 or selectedIndex > #languageCodes then
+    return languageCodes[1][1]
+  else
+    return languageCodes[selectedIndex][1]
+  end
 end
 
 local win = ui.Window("gTranslate", "single", 840*ui.dpi, 480*ui.dpi)
 win.bgcolor = 0x313338
-local button = ui.Button(win, "Przetłumacz", 0, 480*ui.dpi-32*ui.dpi, 840*ui.dpi, 32*ui.dpi)
-local entry = ui.Entry(win, "Wpisz co chcesz przetłumaczyć", 20*ui.dpi, 20*ui.dpi, 380*ui.dpi, 64*ui.dpi)
+local button = ui.Button(win, "Translate", 0, 480*ui.dpi-32*ui.dpi, 840*ui.dpi, 32*ui.dpi)
+local entry = ui.Entry(win, "", 20*ui.dpi, 20*ui.dpi, 380*ui.dpi, 64*ui.dpi)
 local outputEntry = ui.Entry(win, "Translated text here", 420*ui.dpi, 20*ui.dpi, 400*ui.dpi, 64*ui.dpi)
 ui.theme = "dark"
 outputEntry.enabled = false
+
 
 local combo = ui.Combobox(win, {
     languageCodes[1][2],
@@ -45,12 +49,6 @@ local combo = ui.Combobox(win, {
     languageCodes[8][2],
     languageCodes[9][2],
 }, 20*ui.dpi, 84*ui.dpi, 128*ui.dpi, 128*ui.dpi)
-for i, v in ipairs(languageCodes) do
-  combo.items[i]:loadicon(i..".ico")
-end
-combo.style = "icons"
-combo.selected = combo.items[1]
-
 local combo2 = ui.Combobox(win, {
     languageCodes[1][2],
     languageCodes[2][2],
@@ -63,13 +61,19 @@ local combo2 = ui.Combobox(win, {
     languageCodes[9][2],
 }, 420*ui.dpi, 84*ui.dpi, 128*ui.dpi, 128*ui.dpi)
 for i, v in ipairs(languageCodes) do
+  combo.items[i]:loadicon(i..".ico")
+end
+combo.style = "icons"
+combo.selected = combo.items[1]
+for i, v in ipairs(languageCodes) do
   combo2.items[i]:loadicon(i..".ico")
 end
 combo2.style = "icons"
 combo2.selected = combo2.items[2]
 
 function button:onClick()
-	outputEntry.text = translate(entry.text, "pl", "en")
+  outputEntry.text = "!@#$%^&*()"
+	outputEntry.text = translate(entry.text, getLanguageCode(combo.selected.index), getLanguageCode(combo2.selected.index))
 end
 
 entry.fontsize = math.floor(14*ui.dpi)
